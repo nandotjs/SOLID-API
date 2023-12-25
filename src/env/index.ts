b8977bbd77a811f8b546c0e6144bd22de929f120
nan <ipfilus@gmail.com>
2023-12-24 22:19:52 -0300

    .

--- src/env/index.ts
+++ src/env/index.ts
@@ -16,8 +16,8 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-    console.error('❌Invalid environment variables', _env.error.format())
-    throw new Error('Invalid environment variables')
+    console.error('❌Invalid environment variables.', _env.error.format())
+    throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
