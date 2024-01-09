14ac19c55269e8e9143eba79ca153092d1931693
nan <ipfilus@gmail.com>
2024-01-09 02:07:11 -0300

    Feat: updating app.ts and adding another prisma file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
 import fastify from "fastify"
-import { PrismaClient } from "@prisma/client"
+import { z } from 'zod'
+import { prisma } from "./env/lib/prisma"
 
 export const app = fastify()
-const prisma = new PrismaClient()
+
+app.post('/user', async (req, rep) => {
+    const RegisterBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const { name, email, password } = RegisterBodySchema.parse(req.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    })
+
+    return rep.status(201).send()
+})
